c6024424adaea9d9d4bc5fc1d27a2837968abe17
Md Sajedul Islam <mdsajedulra@gmail.com>
2025-01-04 01:45:54 +0600

    second time

--- src/components/ui/Headers.tsx
+++ src/components/ui/Headers.tsx
@@ -1,9 +1,12 @@
+import { Link } from "react-router-dom";
 
 const Headers = () => {
     return (
-        <div className="flex">
+        <div className="flex justify-evenly bg-blue-700 text-white p-5">
             <div>Logo</div>
-            <div>Menu</div>
+            <div>
+                <Link to={''}></Link>
+            </div>
             <div>Hamburger</div>
         </div>
     );
